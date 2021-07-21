1dcb78d120e7771f6579d73431812f9314621672
Anton Patsev <patsev.anton@gmail.com>
2021-07-21 12:46:20 +0600

    echo "ls --color=auto /data:"

--- commands.sh
+++ commands.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-RUN mkdir -p /data/agent
+echo "run commands.sh"
+
+echo "ls --color=auto /data:"
+ls --color=auto /data
+
+echo "mkdir -p /data/agent:"
+mkdir -p /data/agent
 
 echo "ls --color=auto /data-download/agent:"
 ls --color=auto /data-download/agent
