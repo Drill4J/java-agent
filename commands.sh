0c1c667354614325a2c391d72cb6d27850941e2c
Anton Patsev <patsev.anton@gmail.com>
2021-07-21 18:52:03 +0600

    EPMDJ-8028 Download java-agent, store java-agent into kubernetes

--- commands.sh
+++ commands.sh
@@ -1,4 +1,35 @@
 #!/bin/bash
 
-cp -R /data-download/* /data
-nginx -g 'daemon off;'
+echo "run commands.sh"
+echo ""
+
+echo "list in directory /data/agent:"
+ls --color=auto /data/agent || true
+echo ""
+
+echo "mkdir -p /data/agent"
+mkdir -p /data/agent
+sleep 1
+echo ""
+
+echo "list in directory /data-download/agent:"
+ls --color=auto /data-download/agent
+echo ""
+
+if [ -z "$AGENT_VERSION" ]
+then
+      echo "Variable $AGENT_VERSION is empty"
+      echo "Used agent version from docker image"
+      echo "cp -R /data-download/agent/* /data/agent:"
+      cp -R /data-download/agent/* /data/agent
+      echo ""
+else
+      echo "Variable $AGENT_VERSION is NOT empty"
+      echo "Download agent version from internet"
+      echo "run download-artifact.sh"
+      ./download-artifact.sh
+fi
+
+echo "list in directory /data/agent:"
+ls --color=auto /data/agent
+echo ""
