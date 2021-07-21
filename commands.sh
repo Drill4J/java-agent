36c1a616354c6bea01d984c62c1f56220a37af2a
Anton Patsev <patsev.anton@gmail.com>
2021-07-21 12:49:17 +0600

    echo "list in directory /data:"

--- commands.sh
+++ commands.sh
@@ -2,16 +2,16 @@
 
 echo "run commands.sh"
 
-echo "ls --color=auto /data:"
+echo "list in directory /data:"
 ls --color=auto /data
 
 echo "mkdir -p /data/agent:"
 mkdir -p /data/agent
 
-echo "ls --color=auto /data-download/agent:"
+echo "list in directory /data-download/agent:"
 ls --color=auto /data-download/agent
 
 cp -R /data-download/agent/* /data/agent
 
-echo "ls --color=auto /data/agent:"
+echo "list in directory /data/agent:"
 ls --color=auto /data/agent
