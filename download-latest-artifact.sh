706182bd23893dd1e5449739c7a1bad51ce57984
Anton Patsev <patsev.anton@gmail.com>
2021-07-21 16:26:23 +0600

    grep '<latest>'

--- download-latest-artifact.sh
+++ download-latest-artifact.sh
@@ -17,8 +17,8 @@ version=$(curl -s $path/maven-metadata.xml | grep latest | sed "s/.*<latest>\([^
 echo "version:"
 echo $version
 echo "path/maven-metadata.xml:"
-echo $path//maven-metadata.xml
-build=$(curl -s $path//maven-metadata.xml | grep '<value>' | head -1 | sed "s/.*<value>\([^<]*\)<\/value>.*/\1/")
+echo $path/maven-metadata.xml
+build=$(curl -s $path/maven-metadata.xml | grep '<latest>' | head -1 | sed "s/.*<value>\([^<]*\)<\/value>.*/\1/")
 echo "build:"
 echo $build
 jar=$name-$build.jar
