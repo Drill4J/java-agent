714265c682ab22283c9fb653229ffc97a15a0b70
LikhanovMaxim <LikhanovMaxim@yandex.ru>
2021-09-06 14:16:00 +0300

    EPMDJ-8111 Support kafka for autotest agent changed instrumentation of consumer for Spring

--- src/commonNative/kotlin/com/epam/drill/agent/instrument/KafkaTransformer.kt
+++ src/commonNative/kotlin/com/epam/drill/agent/instrument/KafkaTransformer.kt
@@ -17,9 +17,6 @@ package com.epam.drill.agent.instrument
 
 actual object KafkaTransformer {
 
-    const val KAFKA_PRODUCER_INTERFACE = "org/apache/kafka/clients/producer/Producer"
-    const val KAFKA_CONSUMER_INTERFACE = "org/apache/kafka/clients/consumer/Consumer"
-
     actual fun transform(
         className: String,
         classfileBuffer: ByteArray,
