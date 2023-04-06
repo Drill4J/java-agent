d31fdc23911404eddfdb0f11e47aaa90053056a0
epamrsa <epamrsa@gmail.com>
2023-04-06 17:55:05 +0300

    refactor: migration to lib-jvm-shared
    Adjust build scripts gradle plugins and dependencies
    
    Refs: EPMDJ-10572

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,7 @@
-@Suppress("RemoveRedundantBackticks")
 plugins {
-    `java-gradle-plugin`
-    `maven-publish`
-    `distribution`
-    `java`
-    `application`
     kotlin("jvm").apply(false)
     kotlin("multiplatform").apply(false)
     kotlin("plugin.serialization").apply(false)
-    id("io.github.gradle-nexus.publish-plugin")
     id("com.github.hierynomus.license").apply(false)
     id("com.github.johnrengelman.shadow").apply(false)
     id("com.epam.drill.gradle.plugin.kni").apply(false)
@@ -17,7 +10,36 @@ plugins {
 
 group = "com.epam.drill"
 
+val kotlinVersion: String by extra
+val kotlinxCollectionsVersion: String by extra
+val kotlinxCoroutinesVersion: String by extra
+val kotlinxSerializationVersion: String by extra
+
 repositories {
     mavenLocal()
     mavenCentral()
+}
+
+subprojects {
+    val constraints = setOf(
+        dependencies.constraints.create("org.jetbrains.kotlin:kotlin-reflect:$kotlinVersion"),
+        dependencies.constraints.create("org.jetbrains.kotlin:kotlin-stdlib:$kotlinVersion"),
+        dependencies.constraints.create("org.jetbrains.kotlin:kotlin-stdlib-common:$kotlinVersion"),
+        dependencies.constraints.create("org.jetbrains.kotlin:kotlin-stdlib-jdk7:$kotlinVersion"),
+        dependencies.constraints.create("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlinVersion"),
+        dependencies.constraints.create("org.jetbrains.kotlinx:kotlinx-collections-immutable:$kotlinxCollectionsVersion"),
+        dependencies.constraints.create("org.jetbrains.kotlinx:kotlinx-collections-immutable-jvm:$kotlinxCollectionsVersion"),
+        dependencies.constraints.create("org.jetbrains.kotlinx:kotlinx-coroutines-core:$kotlinxCoroutinesVersion"),
+        dependencies.constraints.create("org.jetbrains.kotlinx:kotlinx-coroutines-core-jvm:$kotlinxCoroutinesVersion"),
+        dependencies.constraints.create("org.jetbrains.kotlinx:kotlinx-coroutines-debug:$kotlinxCoroutinesVersion"),
+        dependencies.constraints.create("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:$kotlinxCoroutinesVersion"),
+        dependencies.constraints.create("org.jetbrains.kotlinx:kotlinx-serialization-core:$kotlinxSerializationVersion"),
+        dependencies.constraints.create("org.jetbrains.kotlinx:kotlinx-serialization-core-jvm:$kotlinxSerializationVersion"),
+        dependencies.constraints.create("org.jetbrains.kotlinx:kotlinx-serialization-json:$kotlinxSerializationVersion"),
+        dependencies.constraints.create("org.jetbrains.kotlinx:kotlinx-serialization-json-jvm:$kotlinxSerializationVersion"),
+        dependencies.constraints.create("org.jetbrains.kotlinx:kotlinx-serialization-protobuf:$kotlinxSerializationVersion"),
+    )
+    configurations.all {
+        dependencyConstraints += constraints
+    }
 }
