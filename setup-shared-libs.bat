d7e37de30664f41092e6cb06ed5f0430df6d1821
epamrsa <epamrsa@gmail.com>
2023-04-14 17:29:29 +0300

    refactor: migration to lib-jvm-shared
    Add scripts for shared-libs setup
    
    Refs: EPMDJ-10564

--- setup-shared-libs.bat
+++ setup-shared-libs.bat
@@ -1,3 +1,3 @@
 rmdir /s /q lib-jvm-shared
-git clone https://github.com/Drill4J/lib-jvm-shared lib-jvm-shared
+git clone https://github.com/Drill4J/lib-jvm-shared lib-jvm-shared --branch alpine-support/no-http-hook
 gradlew :updateSharedLibs
